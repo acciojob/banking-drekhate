ce21ab97f46c3d9324f166f657e365e780ed65ff
Dipak Rekhate <drekhate@gmail.com>
2023-11-05 09:38:48 +0530

    modification

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,6 +6,10 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
+    public BankAccount() {
+
+    }
+
     public BankAccount(String name, double balance, double minBalance) {
 
     }
@@ -14,17 +18,42 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+        if (digits == 0) {
+            if (sum == 0) {
+                return "";
+            } else {
+                throw new IllegalArgumentException("Account Number can not be generated");
+            }
+        } else if (digits * 9 < sum || digits > sum) {
+            throw new IllegalArgumentException("Account Number can not be generated");
+        } else {
+            for (int digit = 0; digit <= 9; digit++) {
+                try {
+                    String newNumber = generateAccountNumber(digits - 1, sum - digit);
+                    if (newNumber != null) {
+                        return digit + newNumber;
+                    }
+                } catch (IllegalArgumentException e) {
+                    continue;
+                }
+            }
+            throw new IllegalArgumentException("Account Number can not be generated");
+        }
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance += amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if(amount <= balance) {
+            balance -= amount;
+        } else {
+            throw new RuntimeException("Insufficient Balance");
+        }
 
     }
 
